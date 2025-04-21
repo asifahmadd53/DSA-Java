7721442e4d46adfc8ba13cc8f1d3ecfd3ce1477a
asifahmadd53 <asifahmadd53@gmail.com>
2025-04-21 22:59:38 +0500

    folder structire

---
+++ src/Basics/Variables.java
@@ -0,0 +1,12 @@
+package Basics;
+import java.util.*;
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("Brothers Input please");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Your name is "+ name);
+        System.out.println();
+    }
+}
