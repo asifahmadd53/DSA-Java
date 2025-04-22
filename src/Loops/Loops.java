6aa22801733225a5d77b451eef3520dd1f4fe677
asifahmadd53 <asifahmadd53@gmail.com>
2025-04-23 00:00:09 +0500

    table

--- src/Loops/Loops.java
+++ src/Loops/Loops.java
@@ -5,9 +5,11 @@ public class Loops {
     public static void main(String[] args){
 
         System.out.println("Enter the number I gave uh table till 10 ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         for(int i = 1; i<=10; i++){
 //            System.out.println("Helo My name is Asif!!");
-            System.out.println(2*i);
+            System.out.println(n+"*"+i + "="+ n*i);
         }
     }
 }
