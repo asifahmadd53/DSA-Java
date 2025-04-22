52780c69f2adb437856595684b585b89e462ad7e
asifahmadd53 <asifahmadd53@gmail.com>
2025-04-22 23:55:21 +0500

    loops basics

--- src/Loops/Loops.java
+++ src/Loops/Loops.java
@@ -2,8 +2,10 @@ package Loops;
 
 public class Loops {
     public static void main(String[] args){
-        for(int i = 0; i<10; i++){
-            System.out.println("Helo My name is Asif!!");
+
+        for(int i = 1; i<=10; i++){
+//            System.out.println("Helo My name is Asif!!");
+            System.out.println(i++);
         }
     }
 }
