f0adcaa9b55062fa2b8d29865abf0853f5f3cd3b
asifahmadd53 <asifahmadd53@gmail.com>
2025-04-23 01:38:05 +0500

    towards the pattern

--- src/Loops/Patterns.java
+++ src/Loops/Patterns.java
@@ -2,8 +2,16 @@ package Loops;
 
 public class Patterns {
     public static void main(String[] args){
-        for (int i=1; i<=5; i++){
-            System.out.print("*");
+//        for (int i=1; i<=5; i++) {
+//            System.out.print("*");
+//        }
+
+        for( int i = 1; i<=4; i++){
+            for (int j = 1; j<=5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
+
     }
 }
