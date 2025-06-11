29752f8c894b230cc1c7562bb8a04928dbfb50b8
asifahmadd53 <asifahmadd53@gmail.com>
2025-06-11 23:19:12 +0500

    trianglar pyramid

--- src/Loops/Patterns.java
+++ src/Loops/Patterns.java
@@ -39,19 +39,26 @@ public class Patterns {
 //            System.out.println();
 //        }
 
-        int n = 4;
-        int m = 5;
-        for (int i = 1; i<=n; i++){
-            for (int j = 1; j<=5; j++ ){
-                if (i ==1 || j==1 || i==n || j==m){
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+//        int n = 4;
+//        int m = 5;
+//        for (int i = 1; i<=n; i++){
+//            for (int j = 1; j<=5; j++ ){
+//                if (i ==1 || j==1 || i==n || j==m){
+//                    System.out.print("*");
+//                }else{
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
 
+         int n = 4;
+         for (int i = 1; i<=n; i++){
+             for (int j = 1; j<=i; j++){
+                 System.out.print("*");
+             }
+             System.out.println();
+         }
 
 
     }
